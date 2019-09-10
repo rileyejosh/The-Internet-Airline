5f47e79df05780f8af97501bf7b1f186700f5bd8
Joshua Riley <rileyje@pnw.edu>
2019-09-10 03:19:40 -0500

    added FlightDAO.java

---
+++ src/inc/puc/db/FlightDAO.java
@@ -0,0 +1,13 @@
+package inc.puc.db;
+
+/**
+ * The FlightDAO class is responsible for retrieving data from the AirlineDB
+ * database. It retrieves all rows from the Flight table and returns a
+ * collection of Flight objects to the caller.
+ * 
+ * @author Joshua Riley
+ *
+ */
+public class FlightDAO {
+
+}
