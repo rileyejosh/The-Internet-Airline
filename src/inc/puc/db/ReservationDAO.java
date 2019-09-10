2c85e1b17246d7f43fa48621027e6d150e1f3f8a
Joshua Riley <rileyje@pnw.edu>
2019-09-10 03:20:23 -0500

    added ReservationDAO.java

---
+++ src/inc/puc/db/ReservationDAO.java
@@ -0,0 +1,17 @@
+package inc.puc.db;
+
+/**
+ * The ReservationDAO class is responsible for retrieving data from the
+ * AirlineDB database. It retrieves all rows from the Reservation table and
+ * returns a collection of Reservation objects to the caller.
+ * 
+ * @author Joshua Riley
+ *
+ */
+public class ReservationDAO {
+
+	String databaseURL = "jdbc:sqlserver://localhost:1433;databaseName=AirlineDB";
+	String user = "db-puc";
+	String password = "inc";
+	// TODO
+}
