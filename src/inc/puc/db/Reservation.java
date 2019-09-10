c6230f0c78ca308119c2c5e229d2b5fef0bbdcf8
Joshua Riley <rileyje@pnw.edu>
2019-09-10 03:19:59 -0500

    added Reservation.java

---
+++ src/inc/puc/db/Reservation.java
@@ -0,0 +1,13 @@
+package inc.puc.db;
+
+/**
+ * The Reservation model class represents a row in the Reservation table.
+ * 
+ * @author Joshua Riley
+ *
+ */
+public class Reservation {
+
+	// TODO
+
+}
