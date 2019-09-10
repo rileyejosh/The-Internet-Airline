b1260efee8d2ef1a1683258a7f47ccaa07c9cd7c
Joshua Riley <rileyje@pnw.edu>
2019-09-10 03:18:51 -0500

    added CustomerDAO.java

---
+++ src/inc/puc/db/CustomerDAO.java
@@ -0,0 +1,17 @@
+package inc.puc.db;
+
+/**
+ * The CustomerDAO class is responsible for retrieving data from the AirlineDB
+ * database. It retrieves all rows from the Customer table and returns a
+ * collection of Customer objects to the caller.
+ * 
+ * @author Joshua Riley
+ *
+ */
+public class CustomerDAO {
+	String databaseURL = "jdbc:sqlserver://localhost:1433;databaseName=AirlineDB";
+	String user = "db-puc";
+	String password = "inc";
+
+	// TODO
+}
