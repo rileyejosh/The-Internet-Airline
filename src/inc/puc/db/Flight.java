f63290badc1027eb322a05a9fbf74c28584097ed
Joshua Riley <rileyje@pnw.edu>
2019-09-10 03:19:18 -0500

    added Flight.java

---
+++ src/inc/puc/db/Flight.java
@@ -0,0 +1,17 @@
+package inc.puc.db;
+
+/**
+ * The Flight model class represents a row in the Flight table.
+ * 
+ * @author Joshua Riley
+ *
+ */
+public class Flight {
+
+	private int fid, fnumber, classFlight, capacity, available, orig, dest;
+	private String fdate, ftime;
+	private float price;
+
+	// TODO
+
+}
