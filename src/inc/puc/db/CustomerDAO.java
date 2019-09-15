ae06feceef0b18aa9fd15b230829f2b1e5897ea8
Joshua Riley <rileyje@pnw.edu>
2019-09-15 01:28:03 -0500

    modified code

--- src/inc/puc/db/CustomerDAO.java
+++ src/inc/puc/db/CustomerDAO.java
@@ -1,5 +1,13 @@
 package inc.puc.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * The CustomerDAO class is responsible for retrieving data from the AirlineDB
  * database. It retrieves all rows from the Customer table and returns a
@@ -13,5 +21,39 @@ public class CustomerDAO {
 	String user = "db-puc";
 	String password = "inc";
 
-	// TODO
+	public List<Customer> list() throws SQLException, ClassNotFoundException {
+		List<Customer> customers = new ArrayList<Customer>();
+		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		try (Connection connection = DriverManager.getConnection(databaseURL, user, password)) {
+			String sql = "SELECT cid, email, password FROM customer WHERE lower(email) = '<Email>'";
+			Statement statement = connection.createStatement();
+			ResultSet result = statement.executeQuery(sql);
+
+			/*
+			 * while (result.next()) { int id = result.getInt("cityid"); String title =
+			 * result.getString("title"); String state = result.getString("state"); City
+			 * city = new City(id, title, state);
+			 * 
+			 * customers.add(city);
+			 * 
+			 * }
+			 */
+
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+			throw ex;
+		}
+
+		return customers;
+	}
+
+	// main method for debugging
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+		CustomerDAO c = new CustomerDAO();
+		for (int i = 0; i < c.list().size() - 1; i++) {
+
+			// System.out.println(c.list().get(i).getTitle() + c.list().get(i).getState());
+		}
+	}
 }
