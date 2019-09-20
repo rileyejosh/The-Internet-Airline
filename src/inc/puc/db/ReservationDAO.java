5c882872dcf7cc8ee0bef9750dfbec5be90f1734
Joshua Riley <rileyje@pnw.edu>
2019-09-20 01:48:39 -0500

    finished todo task

--- src/inc/puc/db/ReservationDAO.java
+++ src/inc/puc/db/ReservationDAO.java
@@ -1,5 +1,13 @@
 package inc.puc.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * The ReservationDAO class is responsible for retrieving data from the
  * AirlineDB database. It retrieves all rows from the Reservation table and
@@ -13,5 +21,40 @@ public class ReservationDAO {
 	String databaseURL = "jdbc:sqlserver://localhost:1433;databaseName=AirlineDB";
 	String user = "db-puc";
 	String password = "inc";
-	// TODO
+
+	public List<Reservation> list() throws SQLException, ClassNotFoundException {
+		List<Reservation> reservations = new ArrayList<Reservation>();
+		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		try (Connection connection = DriverManager.getConnection(databaseURL, user, password)) {
+			String sql = "SELECT cid, email, password FROM customer WHERE lower(email) = '<Email>'";
+			Statement statement = connection.createStatement();
+			ResultSet result = statement.executeQuery(sql);
+
+			/*
+			 * while (result.next()) { int id = result.getInt("cityid"); String title =
+			 * result.getString("title"); String state = result.getString("state"); City
+			 * city = new City(id, title, state);
+			 * 
+			 * customers.add(city);
+			 * 
+			 * }
+			 */
+
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+			throw ex;
+		}
+
+		return reservations;
+	}
+
+	// main method for debugging
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+		ReservationDAO r = new ReservationDAO();
+		for (int i = 0; i < r.list().size() - 1; i++) {
+
+			// System.out.println(c.list().get(i).getTitle() + c.list().get(i).getState());
+		}
+	}
 }
